dd1a0b09440a3af5f67b0f76568153963ca31d14
unknown <cristianpipe0494@gmail.com>
2019-10-30 11:40:54 -0600

    Have added functionlity to ToDo, now is able to show pendings, completed and all tasks.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,115 @@
-import React from 'react';
+import React, {Component} from 'react';
+import Task from "./Task";
 
-function App() {
-  return (
-    <div>
-      <header>
-        <h2>To Do React</h2>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      tasks: [],
+      dashboard: []
+    }
+    this.createTask=this.createTask.bind(this);
+    this.showAllTasks=this.showAllTasks.bind(this);
+    this.showStatusTasks=this.showStatusTasks.bind(this);
+    this.clearDashboard=this.clearDashboard.bind(this);
+    this.saveTask=this.saveTask.bind(this);
+  }
+
+  componentDidMount() {
+    const taskStored = JSON.parse(localStorage.getItem("TaskStorage"));
+    if (taskStored) {
+      console.log(taskStored)
+      const taskDataStored = taskStored.map((task) => {
+        console.log(task);
+        const dataObject = {
+          task: task.task,
+          stop: task.stop,
+          start: task.start,
+          descript: task.descript,
+          status: task.status
+        }
+        return dataObject;
+      });
+      this.setState({
+        tasks: taskDataStored
+      });
+    }
+  }
+  
+  createTask() {
+    this.setState({
+      dashboard: <Task save={this.saveTask}></Task>
+    })
+  }
+
+  showAllTasks() {
+    const allTasks = this.state.tasks.map((task) => {
+      console.log(task);
+      return <Task task={task.task} stop={task.stop} start={task.start} descript={task.descript} save={this.saveTask} key={task.key} status={task.status}></Task>
+    });
+    this.setState({
+      dashboard: allTasks
+    })
+  }
+
+  showStatusTasks(statuscheckbox) {
+    const statusTasks = this.state.tasks.map((task) => {
+      if(task.status === statuscheckbox) {
+        return <Task task={task.task} stop={task.stop} start={task.start} descript={task.descript} save={this.saveTask} key={task.key} status={task.status}/>
+      }
+    });
+    this.setState({
+      dashboard: statusTasks
+    })
+  }
+
+  clearDashboard() {
+    this.setState({
+      dashboard: ""
+    })
+  }
+
+  saveTask(taskName, deadline, startDate, taskDescript, taskStatus) {
+    this.setState({
+      dashboard: ""
+    }, () => {
+      console.log(taskName, deadline, startDate, taskDescript, taskStatus);
+      const savedData = {
+        task: taskName,
+        stop: deadline,
+        start: startDate,
+        descript: taskDescript,
+        save: this.saveTask,
+        key: `${taskName}-${startDate}`,
+        status: taskStatus
+      }
+      const prevData = this.state.tasks;
+      prevData.push(savedData);
+      this.setState({
+        tasks: prevData
+      }, () => {
+        localStorage.setItem("TaskStorage", JSON.stringify(this.state.tasks))
+      })
+    })
+  }
+
+  render() {
+    {console.log("Data is: ", this.state)}
+    return(
+      <div>
+        <div>
+          <button onClick={this.createTask}>Create new task</button>
+          <button onClick={this.showAllTasks}>All tasks</button>
+          <button onClick={() => this.showStatusTasks(false)}>Pending tasks</button>
+          <button onClick={() => this.showStatusTasks(true)}>Completed tasks</button>
+          <button onClick={this.clearDashboard}>Clear dashboard</button>
+        </div>
+        <div id="dashboard">
+          {this.state.dashboard}
+        </div>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Task.js
@@ -0,0 +1,50 @@
+import React, {useState} from "react";
+
+const Task = ({task, stop, start, descript, status, save}) => {
+  const defaultDate = new Date().toLocaleDateString();
+  const [taskName, setTaskNameHandler] = useState("Default Name.");
+  const [deadline, setDeadlineHandler] = useState(defaultDate);
+  const [startDate, setStartDateHandler] = useState(defaultDate);
+  const [taskDescript, setTaskDescriptHandler] = useState("Default Descript.");
+  const [taskStatus, setTaskStatusHandler] = useState(status !== "" ? status : false);
+  return (
+    <div>
+      <label>
+        Task Name
+        <input type="text" onChange={e => setTaskNameHandler(e.target.value)} id="taskName" value={task !== "" ? task : taskName} />
+      </label>
+
+      <label>
+        Deadline
+        <input type="date" onChange={e => setDeadlineHandler(e.target.value)} id="deadline" value={stop !== "" ? stop : deadline} />
+      </label>
+
+      <label>
+        Started
+        <input type="date" onChange={e => setStartDateHandler(e.target.value)} id="startDate" value={start !== "" ? start : startDate} />
+      </label>
+
+      <label>
+        Task description
+        <input type="text" onChange={e => setTaskDescriptHandler(e.target.value)} id="taskDescript" value={descript !== "" ? descript : taskDescript} />
+      </label>
+
+      <label>
+        {taskStatus ? "Task is completed" : "Task is incompleted"}
+        <input type="checkbox" onChange={() => setTaskStatusHandler(!taskStatus)} id="taskStatus" value={taskStatus} checked={taskStatus}/>
+      </label>
+
+      <button onClick={() => save(taskName, deadline, startDate, taskDescript, taskStatus)}>Save task</button>
+    </div>
+  )
+};
+
+export default Task;
+
+Task.defaultProps = {
+  task: "",
+  stop: "",
+  start: "",
+  descript: "",
+  status: ""
+}
